bba46f22e8cb44c806d0d0383261adde523964df
RupeshKumar4511 <kumarrn4511@gmail.com>
2025-08-13 11:01:50 +0530

    logout feature updated

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const authValidation = require('../Middlewares/AuthValidation');
-const {signUpSchema,signInSchema,resetSchema} = require('../utils/userSchema');
-const {checkSchema} = require('express-validator')
+const { signUpSchema, signInSchema, resetSchema } = require('../utils/userSchema');
+const { checkSchema } = require('express-validator')
 const { signup, signin } = require('../controllers/AuthControllers');
 const verifyOtp = require('../Middlewares/otpVerify');
 const sendOtp = require('../services/mail');
@@ -14,27 +14,33 @@ const routes = express.Router();
 
 
 
-routes.post('/signin',checkSchema(signInSchema), authValidation, signin);
-routes.post('/signup',checkSchema(signUpSchema), authValidation, verifyOtp, signup);
-routes.post('/send-email',checkUser,sendOtp,(req,res)=>{
-    return res.status(200).json({message:"Email sent Successfully..",success:true})
+routes.post('/signin', checkSchema(signInSchema), authValidation, signin);
+routes.post('/signup', checkSchema(signUpSchema), authValidation, verifyOtp, signup);
+routes.post('/send-email', checkUser, sendOtp, (req, res) => {
+  return res.status(200).json({ message: "Email sent Successfully..", success: true })
 });
-routes.post('/verify-email',verifyOtp,(req,res)=>{
-    return res.status(200).json({message:"Email verified Successfully..",success:true})
+routes.post('/verify-email', verifyOtp, (req, res) => {
+  return res.status(200).json({ message: "Email verified Successfully..", success: true })
 });
-routes.post('/reset-password',checkSchema(resetSchema),authValidation, verifyOtp, resetPassword);
+routes.post('/reset-password', checkSchema(resetSchema), authValidation, verifyOtp, resetPassword);
 
-routes.post('/sign-out',ensureAuthenticated,(req,res)=>{
-  const {username}= req.body;
-    if(req.user.username !== username){
-      return res.status(400).send({message:"Bad Request",
-      success:false,
-      username})
-    }
+routes.post('/sign-out', ensureAuthenticated, (req, res) => {
+  const { username } = req.body;
+  if (req.user.username !== username) {
+    return res.status(400).send({
+      message: "Bad Request",
+      success: false,
+      username
+    })
+  }
 
-    
-    res.clearCookie('token')
-    return res.status(200).send({message:"Logout Successfully",logout:true})
+
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'None',
+  });
+  return res.status(200).send({ message: "Logout Successfully", logout: true })
 
 
 })
