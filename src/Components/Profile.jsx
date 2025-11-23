38826841064a343b39ebaed4a543f3d960e2ead3
RupeshKumar4511 <kumarrn4511@gmail.com>
2025-11-23 10:55:27 +0530

    profile updated

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,30 +1,34 @@
 import { useSelector } from "react-redux";
 import ErrorPage from './ErrorPage';
+import { CgProfile } from "react-icons/cg";
 
 const Profile = () => {
 
-  const {authResponse}= useSelector((store)=>store.auth)
+  const { authResponse } = useSelector((store) => store.auth)
 
-  if(!authResponse.success || authResponse.logout){
+  if (!authResponse.success || authResponse.logout) {
     return (
-      <ErrorPage/>
+      <ErrorPage />
     )
   }
 
   function getUserName(user) {
-        const username = user.charAt(0).toUpperCase() + user.slice(1);
-        return username;
-    }
-  
+    const username = user.charAt(0).toUpperCase() + user.slice(1);
+    return username;
+  }
+
   return (
     <div className="flex justify-center w-full my-5 ">
       <div className="flex flex-col gap-5 bg-white w-[80%] md:w-[60%] lg:w-[50%] h-50 py-5  shadow-md">
         <p className="flex text-xl md:text-2xl font-bold text-blue-800 justify-center">Your Profile</p>
         <div className="flex flex-col mx-auto gap-4 px-5">
-            <p className="md:text-xl font-medium">Username : <span className="text-blue-500">{getUserName(authResponse.username)}</span></p>
-            <p className="md:text-xl font-medium">Email Id : <span className="text-blue-500">{(authResponse).email}</span></p>
-            {/* <p className="md:text-xl font-medium">Role : <span className="text-blue-500">{response.role}</span></p> */}
-            
+          <div className="flex justify-center">
+            <CgProfile size={50} />
+          </div>
+          <p className="md:text-xl font-medium">Username : <span className="text-blue-500">{getUserName(authResponse.username)}</span></p>
+          <p className="md:text-xl font-medium">Email Id : <span className="text-blue-500">{(authResponse).email}</span></p>
+          {/* <p className="md:text-xl font-medium">Role : <span className="text-blue-500">{response.role}</span></p> */}
+
         </div>
       </div>
     </div>
