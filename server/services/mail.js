4db4a7d3c998c2090114ee9da0bdb4b0f7523cac
RupeshKumar4511 <kumarrn4511@gmail.com>
2025-11-25 11:49:28 +0530

    mail.js updated

--- server/services/mail.js
+++ server/services/mail.js
@@ -5,16 +5,16 @@ const fetch = require('node-fetch');
 require('dotenv').config()
 const sgMail = require("@sendgrid/mail");
 
-async function verifyEmail(email) {
-    try {
-        const apiKey = process.env.ZEROBOUNCE_API_KEY;
-        const res = await fetch(`https://api.zerobounce.net/v2/validate?api_key=${apiKey}&email=${encodeURIComponent(email)}`);
-        const data = await res.json();
-        return data.status === "valid";
-    } catch (error) {
-        return false;
-    }
-}
+// async function verifyEmail(email) {
+//     try {
+//         const apiKey = process.env.ZEROBOUNCE_API_KEY;
+//         const res = await fetch(`https://api.zerobounce.net/v2/validate?api_key=${apiKey}&email=${encodeURIComponent(email)}`);
+//         const data = await res.json();
+//         return data.status === "valid";
+//     } catch (error) {
+//         return false;
+//     }
+// }
 
 // const transporter = nodemailer.createTransport({
 //     host: "smtp.sendgrid.net",
@@ -30,10 +30,10 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendOtp = async (req, res, next) => {
 
     const { email } = req.body;
-    const result = await verifyEmail(email);
-    if (!result) {
-        return res.status(400).json({ message: "This email does not exist", success: false });
-    }
+    // const result = await verifyEmail(email);
+    // if (!result) {
+    //     return res.status(400).json({ message: "This email does not exist", success: false });
+    // }
 
     const otp = Math.floor(100000 + Math.random() * 900000);
 
