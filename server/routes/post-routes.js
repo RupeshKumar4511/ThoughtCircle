76cbee500f7a95f0cc373b9910ad9b3a8534756a
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-02-27 19:59:34 +0530

    bug fixed

--- server/routes/post-routes.js
+++ server/routes/post-routes.js
@@ -3,6 +3,7 @@ const ensureAuthenticated = require('../Middlewares/Auth');
 const routes = express.Router();
 const { fetchUser } = require('../controllers/AuthControllers');
 const { createPost, fetchPosts, fetchUserPost, updateUserPost, updatePostReaction, deleteUserPost, deleteAnyPost } = require('../controllers/postController');
+const { checkSchema } = require('express-validator')
 const restrictTo = require('../Middlewares/restrictTo.js')
 const {createPostSchema,updatePostSchema,deletePostSchema} = require('../utils/postSchema.js')
 const postDataValidation = require('../Middlewares/postDataValidation.js')
@@ -10,18 +11,18 @@ const postDataValidation = require('../Middlewares/postDataValidation.js')
 
 routes.get('/user', ensureAuthenticated, fetchUser)
 
-routes.post('/user/create-post', ensureAuthenticated, restrictTo(['admin','user']),createPostSchema, postDataValidation, createPost)
+routes.post('/user/create-post', ensureAuthenticated, restrictTo(['admin','user']),checkSchema(createPostSchema), postDataValidation, createPost)
 
 routes.get('/user/posts', ensureAuthenticated, restrictTo(['admin','user']), fetchPosts)
 
 routes.get('/user/user-posts', ensureAuthenticated, restrictTo(['admin','user']), fetchUserPost)
 
-routes.put('/user/user-posts/:_id', ensureAuthenticated, restrictTo(['admin','user']),updatePostSchema, postDataValidation, updateUserPost)
+routes.put('/user/user-posts/:_id', ensureAuthenticated, restrictTo(['admin','user']),checkSchema(updatePostSchema), postDataValidation, updateUserPost)
 
 routes.patch('/user/posts/:id', ensureAuthenticated, restrictTo(['admin','user']), updatePostReaction)
 
-routes.delete('/user/user-posts/:id', ensureAuthenticated, restrictTo(['admin','user']),deletePostSchema, postDataValidation, deleteUserPost)
+routes.delete('/user/user-posts/:id', ensureAuthenticated, restrictTo(['admin','user']),checkSchema(deletePostSchema), postDataValidation, deleteUserPost)
 
-routes.delete('/user/any-posts/:id', ensureAuthenticated, restrictTo(['admin']),deletePostSchema, postDataValidation, deleteAnyPost)
+routes.delete('/user/any-posts/:id', ensureAuthenticated, restrictTo(['admin']),checkSchema(deletePostSchema), postDataValidation, deleteAnyPost)
 
 module.exports = routes
